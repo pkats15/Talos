d4d6adadb485c1ee2029b65b63252cdf3259086f
pkats15 <pkats13@gmail.com>
2016-04-07 12:50:20 +0300

    Added arduino potentiometer

---
+++ Arduino/Pote/Pote.ino
@@ -0,0 +1,25 @@
+#define THRESHOLD 8
+#define PORT 0
+typedef enum {
+  ABOVE,
+  BELOW
+} State;
+State lastState = BELOW;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int v = analogRead(PORT);
+  int mappedV = map(v, 0, 1024, 0, 100);
+  if(mappedV >= THRESHOLD && lastState == BELOW){
+    Serial.println("A");
+    lastState = ABOVE;
+  }else if(mappedV < THRESHOLD && lastState == ABOVE){
+    Serial.println("B");
+    lastState = BELOW;
+  }
+  delay(20);
+}
