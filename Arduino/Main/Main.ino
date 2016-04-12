46a5b635b9cdafe983b113e63be5d975de7ea54f
pkats15 <pkats13@gmail.com>
2016-04-12 12:27:33 +0300

    Added main program

---
+++ Arduino/Main/Main.ino
@@ -0,0 +1,38 @@
+#include <Wire.h>
+int slaveAddress = 0x04;
+int metalDetector = 0x00;
+void handleEV3();
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.begin(slaveAddress);
+  Wire.onRequest(handleEV3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+bool hasMetal() {
+  return analogRead(metalDetector);
+}
+
+int getWeight() {
+  
+}
+
+void handleEV3() {
+  bool metal = hasMetal();
+  int weight1 = getWeight();
+  int weight2;
+  double totalWeight;
+  Wire.write(metal);
+  if(metal) {
+    delay(8000);
+    totalWeight = map(weight2-weight1,0,1024,0,500)/1000.0;
+    Serial.print(String(totalWeight)+"\0");
+  }
+}
+
